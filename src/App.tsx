3ab08a3b83de6dca14788bec4b632ed07f0de818
unknown <ruibing>
2026-08-30 23:17:35 -0400

    added info

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,25 @@ import './App.css'
 
 function App() {
   return (
-    <div className="hero">
-      <h1>hi, i'm ruibing</h1>
-    </div>
+    <main>
+      <section className="hero">
+        <h1>hi, i'm ruibing</h1>
+      </section>
+
+      <section className="hero">
+        <h1>i like to do many things:  <br /> </h1>
+        <p>
+          <a href="#reading">reading</a> (especially visual novels), networking,
+          working out, gaming, cleaning (?)
+        </p>
+      </section>
+
+      <section className="reading" id="reading">
+        <h2>in no particular order: </h2>
+
+        <p> Danganronpa, Science Adv (Steins;Gate, Chaos;Head, Robotics;Notes), Ace Attorney, Umineko </p>
+      </section>
+    </main>
   )
 }
 
